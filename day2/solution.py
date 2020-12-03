e57dc0a6b7261f2671dc638e14a916a21c5d6156
PPitson <pitu5599@gmail.com>
2020-12-03 18:52:06 +0100

    Add solution of day 2 part two

--- day2/solution.py
+++ day2/solution.py
@@ -1,39 +1,67 @@
+import abc
 import re
 from dataclasses import dataclass
-from typing import Iterator
+from typing import Iterator, Type
 
 
 @dataclass
-class Policy:
-    lower_bound: int
-    upper_bound: int
+class Policy(abc.ABC):
+    first_number: int
+    second_number: int
     letter: str
 
+    @abc.abstractmethod
     def is_password_valid(self, password: str) -> bool:
+        pass
+
+
+class LetterOcurrencesInRangePolicy(Policy):
+    def is_password_valid(self, password: str) -> bool:
+        lower_bound, upper_bound = self.first_number, self.second_number
         letter_ocurrences = password.count(self.letter)
-        return self.lower_bound <= letter_ocurrences <= self.upper_bound
+        return lower_bound <= letter_ocurrences <= upper_bound
 
 
-def valid_passwords_count(input_filename: str) -> int:
-    return sum(1 for policy, password in _parse_file(input_filename) if policy.is_password_valid(password))
+class LetterOnOneOfPositionsPolicy(Policy):
+    def is_password_valid(self, password: str) -> bool:
+        first_position = self.first_number - 1
+        second_position = self.second_number - 1
+        return (password[first_position] == self.letter and password[second_position] != self.letter) or (
+            password[first_position] != self.letter and password[second_position] == self.letter
+        )
 
 
-def _parse_file(input_filename: str) -> Iterator[tuple[Policy, str]]:
+def part_one(input_filename: str) -> int:
+    return valid_passwords_count(input_filename, LetterOcurrencesInRangePolicy)
+
+
+def part_two(input_filename: str) -> int:
+    return valid_passwords_count(input_filename, LetterOnOneOfPositionsPolicy)
+
+
+def valid_passwords_count(input_filename: str, policy_factory: Type[Policy]) -> int:
+    return sum(
+        1 for policy, password in _parse_file(input_filename, policy_factory) if policy.is_password_valid(password)
+    )
+
+
+def _parse_file(input_filename: str, policy_factory: Type[Policy]) -> Iterator[tuple[Policy, str]]:
     with open(input_filename) as file:
         for line in file:
-            yield _parse_line(line)
+            yield _parse_line(line, policy_factory)
 
 
-def _parse_line(line: str) -> tuple[Policy, str]:
-    match = re.match(r"(?P<lower_bound>\d+)-(?P<upper_bound>\d+) (?P<letter>[a-z]): (?P<password>[a-z]+)", line)
+def _parse_line(line: str, policy_factory: Type[Policy]) -> tuple[Policy, str]:
+    match = re.match(r"(?P<first_number>\d+)-(?P<second_number>\d+) (?P<letter>[a-z]): (?P<password>[a-z]+)", line)
     group_dict = match.groupdict()
-    policy = Policy(
-        lower_bound=int(group_dict["lower_bound"]),
-        upper_bound=int(group_dict["upper_bound"]),
+    policy = policy_factory(
+        first_number=int(group_dict["first_number"]),
+        second_number=int(group_dict["second_number"]),
         letter=group_dict["letter"],
     )
     return policy, group_dict["password"]
 
 
 if __name__ == "__main__":
-    print(valid_passwords_count("data/input.txt"))
+    print(part_one("data/input.txt"))
+    print(part_two("data/input.txt"))
--- day2/test_solution.py
+++ day2/test_solution.py
@@ -1,5 +1,9 @@
-from day2.solution import valid_passwords_count
+from day2.solution import part_one, part_two
 
 
-def test_passwords_valid():
-    assert valid_passwords_count("data/test_input.txt") == 2
+def test_passwords_valid_part_one():
+    assert part_one("data/test_input.txt") == 2
+
+
+def test_passwords_valid_part_two():
+    assert part_two("data/test_input.txt") == 1
