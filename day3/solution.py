90f825131f12baa5be33e1e5512258448d5fad61
PPitson <pitu5599@gmail.com>
2020-12-03 20:59:49 +0100

    Add solution of day 3 part two

--- day3/solution.py
+++ day3/solution.py
@@ -1,9 +1,22 @@
+import operator
+from functools import reduce
+
 TREE = "#"
 
 
 def part_one(input_filename: str) -> int:
     area_map = _read_area_map(input_filename)
     return _count_trees(area_map, positions_to_move_right=3, positions_to_move_down=1)
+
+
+def part_two(input_filename: str) -> int:
+    area_map = _read_area_map(input_filename)
+    move_rules = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    trees = (
+        _count_trees(area_map, positions_to_move_right, positions_to_move_down)
+        for positions_to_move_right, positions_to_move_down in move_rules
+    )
+    return reduce(operator.mul, trees, 1)
 
 
 def _read_area_map(input_filename: str) -> list[str]:
@@ -24,3 +37,4 @@ def _count_trees(area_map: list[str], positions_to_move_right: int, positions_to
 
 if __name__ == "__main__":
     print(part_one("data/input.txt"))
+    print(part_two("data/input.txt"))
--- day3/test_solution.py
+++ day3/test_solution.py
@@ -1,5 +1,9 @@
-from day3.solution import part_one
+from day3.solution import part_one, part_two
 
 
 def test_solution_part_one():
     assert part_one("data/test_input.txt") == 7
+
+
+def test_solution_part_two():
+    assert part_two("data/test_input.txt") == 336
