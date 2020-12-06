8fb89a346230639500812afc6bfe54f7845658c3
PPitson <pitu5599@gmail.com>
2020-12-06 17:18:29 +0100

    Add solution of day 6 part two

--- day6/solution.py
+++ day6/solution.py
@@ -5,6 +5,13 @@ def part_one(input_filename: str) -> int:
     return sum(_count_unique_answers_in_group(answers_in_group) for answers_in_group in _parse_file(input_filename))
 
 
+def part_two(input_filename: str) -> int:
+    return sum(
+        _count_questions_answered_by_every_person_in_group(answers_in_group)
+        for answers_in_group in _parse_file(input_filename)
+    )
+
+
 def _parse_file(input_filename: str) -> Iterator[str]:
     with open(input_filename) as file:
         all_answers = file.read()
@@ -16,5 +23,11 @@ def _count_unique_answers_in_group(answers_in_group: str) -> int:
     return len(set(answers_in_group.replace("\n", "")))
 
 
+def _count_questions_answered_by_every_person_in_group(answers_in_group: str) -> int:
+    answers_by_person = (set(answers) for answers in answers_in_group.split("\n") if answers.strip())
+    return len(set.intersection(*answers_by_person))
+
+
 if __name__ == "__main__":
     print(part_one("data/input.txt"))
+    print(part_two("data/input.txt"))
--- day6/test_solution.py
+++ day6/test_solution.py
@@ -1,5 +1,9 @@
-from day6.solution import part_one
+from day6.solution import part_one, part_two
 
 
 def test_solution_part_one():
     assert part_one("data/test_input.txt") == 11
+
+
+def test_solution_part_two():
+    assert part_two("data/test_input.txt") == 6
