2d708285c617d3159932f6ee8324b18194fcb007
PPitson <pitu5599@gmail.com>
2020-12-09 21:33:37 +0100

    Add solution of day 9 part two
    Also, to reuse _find_invalid_number I had to refactor it to accept
    numbers as a list, instead of iterator

--- day9/solution.py
+++ day9/solution.py
@@ -5,18 +5,23 @@ from typing import Iterator, Iterable, Any
 
 def part_one(input_filename: str, preamble_length: int) -> int:
     numbers = _get_numbers(input_filename)
-    return _find_number(numbers, preamble_length=preamble_length)
+    return _find_invalid_number(numbers, preamble_length=preamble_length)
 
 
-def _get_numbers(input_filename: str) -> Iterator[int]:
+def part_two(input_filename: str, preamble_length: int) -> int:
+    numbers = _get_numbers(input_filename)
+    invalid_number = _find_invalid_number(numbers, preamble_length=preamble_length)
+    return _find_encryption_weakness(numbers, invalid_number)
+
+
+def _get_numbers(input_filename: str) -> list[int]:
     with open(input_filename) as file:
-        for line in file:
-            yield int(line)
+        return [int(line) for line in file]
 
 
-def _find_number(numbers: Iterator[int], preamble_length: int) -> int:
+def _find_invalid_number(numbers: list[int], preamble_length: int) -> int:
     deque = collections.deque(_take_first_n_numbers(preamble_length, numbers), maxlen=preamble_length)
-    for number in numbers:
+    for number in numbers[preamble_length:]:
         if not _is_sum_of_pair_of_numbers(number, deque):
             return number
         deque.append(number)
@@ -36,5 +41,20 @@ def _is_sum_of_n_numbers(number: int, numbers: Iterator[int], numbers_combinatio
     )
 
 
+def _find_encryption_weakness(numbers: list[int], invalid_number: int) -> int:
+    numbers_sequence = _find_contiguous_sequence_of_numbers_which_sums_to_desired_sum(numbers, invalid_number)
+    return min(numbers_sequence) + max(numbers_sequence)
+
+
+def _find_contiguous_sequence_of_numbers_which_sums_to_desired_sum(numbers: list[int], desired_sum: int) -> list[int]:
+    return next(
+        numbers[i : i + set_length]
+        for set_length in range(2, len(numbers))
+        for i in range(len(numbers) - set_length + 1)
+        if sum(numbers[i : i + set_length]) == desired_sum
+    )
+
+
 if __name__ == "__main__":
     print(part_one("data/input.txt", preamble_length=25))
+    print(part_two("data/input.txt", preamble_length=25))
--- day9/test_solution.py
+++ day9/test_solution.py
@@ -1,5 +1,9 @@
-from day9.solution import part_one
+from day9.solution import part_one, part_two
 
 
 def test_solution_part_one():
     assert part_one("data/test_input.txt", preamble_length=5) == 127
+
+
+def test_solution_part_two():
+    assert part_two("data/test_input.txt", preamble_length=5) == 62
