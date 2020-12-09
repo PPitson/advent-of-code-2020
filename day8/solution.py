4b89ef902447f200e4bfada2f2f290c507a84414
PPitson <pitu5599@gmail.com>
2020-12-09 20:02:30 +0100

    Add solution of day 8 part two

--- day8/solution.py
+++ day8/solution.py
@@ -1,3 +1,4 @@
+from copy import copy
 from dataclasses import dataclass
 from enum import Enum
 
@@ -14,9 +15,21 @@ class Instruction:
     argument: int
 
 
+@dataclass
+class ProgramExecutionResult:
+    accumulator_value: int
+    infinite_loop: bool
+
+
 def part_one(input_filename: str) -> int:
     instructions = _parse_file(input_filename)
-    return _retrieve_accumulator_value_before_infinite_loop(instructions)
+    execution_result = _execute_program(instructions)
+    return execution_result.accumulator_value
+
+
+def part_two(input_filename: str) -> int:
+    instructions = _parse_file(input_filename)
+    return _get_accumulator_value_after_correcting_program(instructions)
 
 
 def _parse_file(input_filename: str) -> list[Instruction]:
@@ -29,11 +42,16 @@ def _parse_instruction(line: str) -> Instruction:
     return Instruction(kind=kind, argument=int(argument))
 
 
-def _retrieve_accumulator_value_before_infinite_loop(instructions: list[Instruction]) -> int:
+def _execute_program(instructions: list[Instruction]) -> ProgramExecutionResult:
     visited = [False for _ in range(len(instructions))]
     index = 0
+    max_index = len(instructions)
     accumulator_value = 0
-    while not visited[index]:
+    infinite_loop = False
+    while index < max_index:
+        if visited[index]:
+            infinite_loop = True
+            break
         instruction = instructions[index]
         visited[index] = True
         if instruction.kind == InstructionType.JUMP:
@@ -43,8 +61,27 @@ def _retrieve_accumulator_value_before_infinite_loop(instructions: list[Instruct
             index += 1
         else:
             index += 1
-    return accumulator_value
+    return ProgramExecutionResult(accumulator_value=accumulator_value, infinite_loop=infinite_loop)
+
+
+def _get_accumulator_value_after_correcting_program(instructions: list[Instruction]) -> int:
+    instruction_replacements = {
+        InstructionType.JUMP: InstructionType.NO_OPERATION,
+        InstructionType.NO_OPERATION: InstructionType.JUMP,
+    }
+    for index, instruction in enumerate(instructions):
+        if instruction.kind not in instruction_replacements:
+            continue
+
+        instructions_copy = copy(instructions)
+        instructions_copy[index] = Instruction(
+            kind=instruction_replacements[instruction.kind], argument=instruction.argument
+        )
+        execution_result = _execute_program(instructions_copy)
+        if not execution_result.infinite_loop:
+            return execution_result.accumulator_value
 
 
 if __name__ == "__main__":
     print(part_one("data/input.txt"))
+    print(part_two("data/input.txt"))
--- day8/test_solution.py
+++ day8/test_solution.py
@@ -1,5 +1,9 @@
-from day8.solution import part_one
+from day8.solution import part_one, part_two
 
 
 def test_solution_part_one():
     assert part_one("data/test_input.txt") == 5
+
+
+def test_solution_part_two():
+    assert part_two("data/test_input.txt") == 8
