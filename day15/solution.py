0b4e94e96e554f338a3b58d09ed62bd7cef444ed
PPitson <pitu5599@gmail.com>
2020-12-19 17:11:02 +0100

    Add solution of day 15 part one

---
+++ day15/solution.py
@@ -0,0 +1,31 @@
+def part_one(input_filename: str) -> int:
+    starting_numbers = _get_starting_numbers(input_filename)
+    return play_game(starting_numbers, number_of_turns=2020)
+
+
+def _get_starting_numbers(input_filename: str) -> list[int]:
+    with open(input_filename) as file:
+        numbers_string = file.read()
+        return [int(number) for number in numbers_string.split(",")]
+
+
+def play_game(starting_numbers: list[int], number_of_turns: int) -> int:
+    if number_of_turns <= len(starting_numbers):
+        return starting_numbers[number_of_turns - 1]
+
+    seen_numbers = {number: turn for turn, number in enumerate(starting_numbers[:-1], start=1)}
+    last_spoken = starting_numbers[-1]
+    for turn in range(len(starting_numbers) + 1, number_of_turns + 1):
+        previous_number_was_first_time_spoken = last_spoken not in seen_numbers
+        if previous_number_was_first_time_spoken:
+            seen_numbers[last_spoken] = turn - 1
+            last_spoken = 0
+        else:
+            number = turn - 1 - seen_numbers[last_spoken]
+            seen_numbers[last_spoken] = turn - 1
+            last_spoken = number
+    return last_spoken
+
+
+if __name__ == "__main__":
+    print(part_one("data/input.txt"))
---
+++ day15/test_solution.py
@@ -0,0 +1,19 @@
+import pytest
+
+from day15.solution import part_one, play_game
+
+
+@pytest.mark.parametrize(
+    "starting_numbers, expected_result",
+    [("1,3,2", 1), ("2,1,3", 10), ("1,2,3", 27), ("2,3,1", 78), ("3,2,1", 438), ("3,1,2", 1836), ("0,3,6", 436)],
+)
+def test_solution_part_one(tmp_path, starting_numbers, expected_result):
+    input_filename = tmp_path / "test_input.txt"
+    input_filename.write_text(starting_numbers)
+
+    assert part_one(input_filename.as_posix()) == expected_result
+
+
+def test_play_game_when_number_of_turns_is_no_greater_than_starting_numbers_count():
+    assert play_game([6, 11, 3, 10, 9], number_of_turns=5) == 9
+    assert play_game([6, 11, 3, 10, 9], number_of_turns=4) == 10
