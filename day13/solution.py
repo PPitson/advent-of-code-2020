81b0582f59efd6062756a008740893afc4b2dde9
PPitson <pitu5599@gmail.com>
2020-12-19 14:13:25 +0100

    Add solution of day 13 part two (working only on test input)

--- day13/solution.py
+++ day13/solution.py
@@ -1,3 +1,4 @@
+import itertools
 import math
 from dataclasses import dataclass
 from functools import partial
@@ -12,16 +13,27 @@ class Notes:
 
 
 def part_one(input_filename: str) -> int:
-    notes = _parse_notes(input_filename)
+    file_lines = _get_file_lines(input_filename)
+    notes = _parse_notes(file_lines)
     bus_id = _get_earliest_bus_id(notes)
     return _get_number_of_minutes_to_wait_for_bus(notes, bus_id) * bus_id
 
 
-def _parse_notes(input_filename: str) -> Notes:
+def part_two(input_filename: str) -> int:
+    file_lines = _get_file_lines(input_filename)
+    bus_ids = _get_bus_ids(file_lines)
+    return _find_timestamp(bus_ids)
+
+
+def _get_file_lines(input_filename: str) -> list[str]:
     with open(input_filename) as file:
-        earliest_departure_timestamp = int(next(file))
-        bus_ids_in_service = [int(bus_id) for bus_id in next(file).split(",") if bus_id != BUS_OUT_OF_SERVICE]
-        return Notes(earliest_departure_timestamp, bus_ids_in_service)
+        return list(file)
+
+
+def _parse_notes(file_lines: list[str]) -> Notes:
+    earliest_departure_timestamp = int(file_lines[0])
+    bus_ids_in_service = [int(bus_id) for bus_id in file_lines[1].split(",") if bus_id != BUS_OUT_OF_SERVICE]
+    return Notes(earliest_departure_timestamp, bus_ids_in_service)
 
 
 def _get_earliest_bus_id(notes: Notes) -> int:
@@ -37,5 +49,26 @@ def _get_number_of_minutes_to_wait_for_bus(notes: Notes, bus_id: int) -> int:
     return bus_arrival_timestamp - notes.earliest_departure_timestamp
 
 
+def _get_bus_ids(file_lines: list[str]) -> list[int]:
+    return [int(bus_id) if bus_id != BUS_OUT_OF_SERVICE else -1 for bus_id in file_lines[1].split(",")]
+
+
+def _find_timestamp(bus_ids: list[int]) -> int:
+    max_bus_id = max(bus_ids)
+    max_bus_id_index = bus_ids.index(max_bus_id)
+    other_bus_ids_with_time_diff = [
+        (bus_id, index - max_bus_id_index)
+        for index, bus_id in enumerate(bus_ids)
+        if bus_id != -1 and bus_id != max_bus_id
+    ]
+    candidate = next(
+        candidate
+        for candidate in itertools.count(start=max_bus_id, step=max_bus_id)
+        if all((candidate + time_diff) % bus_id == 0 for bus_id, time_diff in other_bus_ids_with_time_diff)
+    )
+    return candidate - max_bus_id_index
+
+
 if __name__ == "__main__":
     print(part_one("data/input.txt"))
+    print(part_two("data/input.txt"))
--- day13/test_solution.py
+++ day13/test_solution.py
@@ -1,5 +1,9 @@
-from day13.solution import part_one
+from day13.solution import part_one, part_two
 
 
 def test_solution_part_one():
     assert part_one("data/test_input.txt") == 295
+
+
+def test_solution_part_two():
+    assert part_two("data/test_input.txt") == 1068781
