1fecbc8d3490d1309be6443d0a235eb3e3288d18
PPitson <pitu5599@gmail.com>
2020-12-29 18:49:20 +0100

    Add solution of day 22 part two

--- day22/solution.py
+++ day22/solution.py
@@ -1,9 +1,18 @@
 from collections import deque
 
+PLAYER_ONE = "Player 1"
+PLAYER_TWO = "Player 2"
+
 
 def part_one(input_filename: str) -> int:
     player_decks = _get_player_decks(input_filename)
     winning_player_deck = play_game(*player_decks)
+    return compute_score(winning_player_deck)
+
+
+def part_two(input_filename: str) -> int:
+    player_decks = _get_player_decks(input_filename)
+    winning_player_deck = play_recursive_game(*player_decks)
     return compute_score(winning_player_deck)
 
 
@@ -39,9 +48,51 @@ def play_round(player_1_deck: deque[int], player_2_deck: deque[int]) -> None:
     deck.append(lower_card)
 
 
+def play_recursive_game(player_1_deck: deque[int], player_2_deck: deque[int]) -> deque[int]:
+    _winner, winning_deck = _play_recursive_game(player_1_deck, player_2_deck, previous_states=set())
+    return winning_deck
+
+
+def _play_recursive_game(
+    player_1_deck: deque[int], player_2_deck: deque[int], previous_states: set[tuple[tuple[int], tuple[int]]],
+) -> tuple[str, deque[int]]:
+    while player_1_deck and player_2_deck:
+        current_state = (tuple(player_1_deck), tuple(player_2_deck))
+        if current_state in previous_states:
+            return PLAYER_ONE, player_1_deck
+
+        previous_states.add(current_state)
+        play_round_of_recursive_game(player_1_deck, player_2_deck)
+
+    return (PLAYER_ONE, player_1_deck) if player_1_deck else (PLAYER_TWO, player_2_deck)
+
+
+def play_round_of_recursive_game(player_1_deck: deque[int], player_2_deck: deque[int]) -> None:
+    player_1_card = player_1_deck.popleft()
+    player_2_card = player_2_deck.popleft()
+    if len(player_1_deck) >= player_1_card and len(player_2_deck) >= player_2_card:
+        winner, _winning_deck = _play_recursive_game(
+            _get_first_n(player_1_deck, player_1_card),
+            _get_first_n(player_2_deck, player_2_card),
+            previous_states=set(),
+        )
+    else:
+        winner = PLAYER_ONE if player_1_card > player_2_card else PLAYER_TWO
+    deck = player_1_deck if winner == PLAYER_ONE else player_2_deck
+    higher_card = player_1_card if winner == PLAYER_ONE else player_2_card
+    lower_card = player_2_card if winner == PLAYER_ONE else player_1_card
+    deck.append(higher_card)
+    deck.append(lower_card)
+
+
+def _get_first_n(deq: deque[int], n: int) -> deque[int]:
+    return deque(list(deq)[:n])
+
+
 def compute_score(deck: deque[int]) -> int:
     return sum(card * multiplier for multiplier, card in enumerate(reversed(deck), start=1))
 
 
 if __name__ == "__main__":
     print(part_one("data/input.txt"))
+    print(part_two("data/input.txt"))
--- day22/test_solution.py
+++ day22/test_solution.py
@@ -1,10 +1,25 @@
 from collections import deque
 
-from day22.solution import part_one, parse_input, play_round, compute_score, play_game
+import pytest
+
+from day22.solution import (
+    part_one,
+    parse_input,
+    play_round,
+    compute_score,
+    play_game,
+    play_recursive_game,
+    play_round_of_recursive_game,
+    part_two,
+)
 
 
 def test_solution_part_one():
     assert part_one("data/test_input.txt") == 306
+
+
+def test_solution_part_two():
+    assert part_two("data/test_input.txt") == 291
 
 
 def test_parse_input():
@@ -40,6 +55,46 @@ def test_play_game():
     winning_player_deck = deque([6, 4, 9, 8, 5, 2])
 
     result = play_game(player_1_deck, player_2_deck)
+
+    assert result == winning_player_deck
+
+
+@pytest.mark.parametrize(
+    "description, player_1_deck, player_2_deck, expected_player_1_deck, expected_player_2_deck",
+    [
+        ("simple higher card wins", [9, 2, 6, 3, 1], [5, 8, 4, 7, 10], [2, 6, 3, 1, 9, 5], [8, 4, 7, 10]),
+        ("subgame winner wins", [4, 9, 8, 5, 2], [3, 10, 1, 7, 6], [9, 8, 5, 2], [10, 1, 7, 6, 3, 4]),
+        ("subgame(s) winner wins", [2, 8, 1], [6, 3, 4, 10, 9, 7, 5], [8, 1], [3, 4, 10, 9, 7, 5, 6, 2]),
+    ],
+)
+def test_play_round_of_recursive_game(
+    description, player_1_deck, player_2_deck, expected_player_1_deck, expected_player_2_deck
+):
+    player_1_deck = deque(player_1_deck)
+    player_2_deck = deque(player_2_deck)
+
+    play_round_of_recursive_game(player_1_deck, player_2_deck)
+
+    assert player_1_deck == deque(expected_player_1_deck)
+    assert player_2_deck == deque(expected_player_2_deck)
+
+
+def test_play_recursive_game():
+    player_1_deck = deque([9, 2, 6, 3, 1])
+    player_2_deck = deque([5, 8, 4, 7, 10])
+    winning_player_deck = deque([7, 5, 6, 2, 4, 1, 10, 8, 9, 3])
+
+    result = play_recursive_game(player_1_deck, player_2_deck)
+
+    assert result == winning_player_deck
+
+
+def test_play_recursive_game_player_one_wins_if_infinite_loop_spotted():
+    player_1_deck = deque([43, 19])
+    player_2_deck = deque([2, 29, 14])
+    winning_player_deck = deque([43, 19])
+
+    result = play_recursive_game(player_1_deck, player_2_deck)
 
     assert result == winning_player_deck
 
