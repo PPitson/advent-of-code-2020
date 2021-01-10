d3642f7380da45306c9ddef451428882e940a74f
PPitson <pitu5599@gmail.com>
2021-01-10 17:30:56 +0100

    Improve day 23 solution

--- day23/solution.py
+++ day23/solution.py
@@ -3,55 +3,54 @@ def part_one(labels_input: str) -> str:
 
 
 def play_game(labels_input: str, moves: int) -> str:
-    cups = list(map(int, labels_input))
-    current_cup_index = 0
+    cups = _create_cups(labels_input)
+    current_cup = int(labels_input[0])
     for i in range(moves):
-        cups, current_cup_index = make_move(cups, current_cup_index)
+        cups, current_cup = make_move(cups, current_cup)
 
     return get_result_label(cups)
 
 
-def make_move(cups: list[int], current_cup_index: int) -> tuple[list[int], int]:
-    current_cup = cups[current_cup_index]
-    picked_cups = _pick_cups_clockwise_of_current_cup(cups, current_cup_index)
-    cups = _remove_picked_cups(cups, picked_cups)
-    destination_cup = _get_destination_cup(cups, current_cup)
-    cups = _place_picked_cups(cups, picked_cups, destination_cup)
-    current_cup_index = _get_new_current_cup_index(cups, current_cup)
-    return cups, current_cup_index
+def _create_cups(labels_input: str) -> dict[int, int]:
+    cups = list(map(int, labels_input))
+    result = {cup: cups[index + 1] for index, cup in enumerate(cups[:-1])}
+    result[cups[-1]] = cups[0]
+    return result
 
 
-def _pick_cups_clockwise_of_current_cup(cups: list[int], current_cup_index: int) -> list[int]:
-    indices = ((current_cup_index + n) % len(cups) for n in range(1, 4))
-    return [cups[index] for index in indices]
+def make_move(cups: dict[int, int], current_cup: int) -> tuple[dict[int, int], int]:
+    first_picked_cup = cups[current_cup]
+    second_picked_cup = cups[first_picked_cup]
+    third_picked_cup = cups[second_picked_cup]
+
+    destination = _get_destination(current_cup, cups, {first_picked_cup, second_picked_cup, third_picked_cup})
+
+    cup_next_to_destination = cups[destination]
+    cup_next_to_third_picked_cup = cups[third_picked_cup]
+    cups[current_cup] = cup_next_to_third_picked_cup
+    cups[destination] = first_picked_cup
+    cups[third_picked_cup] = cup_next_to_destination
+
+    return cups, cups[current_cup]
 
 
-def _remove_picked_cups(cups: list[int], picked_cups: list[int]) -> list[int]:
-    return [label for label in cups if label not in picked_cups]
-
-
-def _get_destination_cup(cups: list[int], current_cup: int) -> int:
+def _get_destination(current_cup: int, cups: dict[int, int], picked_cups: set[int]) -> int:
     candidate = current_cup - 1
     while candidate > 0:
-        if candidate in cups:
+        if candidate not in picked_cups:
             return candidate
         candidate -= 1
 
-    return max(cups)
+    return max(set(cups.values()) - picked_cups)
 
 
-def _place_picked_cups(cups: list[int], picked_cups: list[int], destination_cup: int) -> list[int]:
-    destination_cup_index = cups.index(destination_cup)
-    return cups[: destination_cup_index + 1] + picked_cups + cups[destination_cup_index + 1 :]
-
-
-def _get_new_current_cup_index(cups: list[int], current_cup: int) -> int:
-    return (cups.index(current_cup) + 1) % len(cups)
-
-
-def get_result_label(cups: list[int]) -> str:
-    one_index = cups.index(1)
-    return "".join(str(label) for label in cups[one_index + 1 :]) + "".join(str(label) for label in cups[:one_index])
+def get_result_label(cups: dict[int, int]) -> str:
+    value = cups[1]
+    result = []
+    while value != 1:
+        result.append(str(value))
+        value = cups[value]
+    return "".join(result)
 
 
 if __name__ == "__main__":
--- day23/test_solution.py
+++ day23/test_solution.py
@@ -1,4 +1,4 @@
-from day23.solution import part_one, play_game, make_move, get_result_label
+from day23.solution import part_one, play_game, get_result_label
 
 
 def test_solution_part_one():
@@ -9,14 +9,9 @@ def test_play_game():
     assert play_game("389125467", moves=10) == "92658374"
 
 
-def test_make_move():
-    assert make_move([3, 8, 9, 1, 2, 5, 4, 6, 7], 0) == ([3, 2, 8, 9, 1, 5, 4, 6, 7], 1)
-    assert make_move([3, 2, 8, 9, 1, 5, 4, 6, 7], 1) == ([3, 2, 5, 4, 6, 7, 8, 9, 1], 2)
-
-
 def test_get_result_label():
-    cups = [5, 3, 7, 4, 1, 9, 2, 6]
-    expected_result = "9265374"
+    cups = {5: 8, 8: 3, 3: 7, 7: 4, 4: 1, 1: 9, 9: 2, 2: 6, 6: 5}
+    expected_result = "92658374"
 
     result = get_result_label(cups)
 
